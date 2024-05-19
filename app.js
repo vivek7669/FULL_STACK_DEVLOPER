8b7b01aebdab5a0e63d7638f35c8dd1b352a88cd
vivek7669 <chauhanvivek0918@gmail.com>
2024-05-19 17:02:30 +0530

    dice program in js = https://6649df750d17a8a4ec5c2dbd--snazzy-cannoli-5c3a4e.netlify.app/

--- app.js
+++ app.js
@@ -1,80 +1,49 @@
-let outer_part = document.querySelector('.outer');
-let inner_part = document.querySelector('.inner');
-let number = [];
-let second_number = [];
-let ampm ;
+dice = document.querySelector('.dies');
+roll_btn = document.querySelector('.roll');
 
+const rolling = (random) => {
+ 
+    dice.style.animation = 'move 4s';
 
-for(let i=1;i<=12;i++){
-    number.push( `<span style="--index:${i}"> <p> ${i} </p> </span>` );
+    setTimeout(() => {
+        
+        switch (random){
+            case 1 : 
+            dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+            break;
+            case 2 : 
+            dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+            break;
+            case 3 : 
+            dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+            break;
+            case 4 : 
+            dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+            break;
+            case 5 : 
+            dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+            break;
+            case 6 : 
+            dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+            break;
+            default:
+                break;
+        }
+        dice.style.animation = 'none';
+    }, 4040);
 }
-for(let i=2;i<=61;i++){
-    second_number.push( `<span class="till" style="--sindex:${i}"><p></p></span>` );
-}
-inner_part.insertAdjacentHTML('beforeend',number.join(''));
-outer_part.insertAdjacentHTML('beforeend',second_number.join(''));
 
-const time = () =>{
-    let date = new Date();
-    let hours = date.getHours() * 30;
-    let minutes = date.getMinutes() * 6;
-    let seconds = date.getSeconds() *6;
+const random_gen = () => {
+    let random = Math.round(Math.random() * 10);
 
-    let hourp = document.querySelector('.had-hour'); 
-    let minutep = document.querySelector('.had-minute'); 
-    let secondp = document.querySelector('.had-second'); 
-
-    hourp.style.transform = `rotate( calc( ${hours + minutes / 12}deg )  ) `;
-    minutep.style.transform = `rotate( calc(${minutes}deg ) ) `;
-    secondp.style.transform = `rotate( calc(${seconds}deg) ) `;
-
-}
-const timetext = () =>{
-    let date = new Date();
-    let hours = date.getHours();
-    let dates = date.getDate();
-    let day = date.getDay();
-
-    switch(day){
-        case 0 : 
-        day = 'Sun';
-        break;
-        case 1 : 
-        day = 'Mon';
-        break;
-        case 2 : 
-        day = 'Tue';
-        break;
-        case 3 : 
-        day = 'Wed';
-        break;
-        case 4 : 
-        day = 'Thu';
-        break;
-        case 5 : 
-        day = 'Fri';
-        break;
-        case 6 : 
-        day = 'Sat';
-        break;
-
+    if(random > 0 && random < 7){
+        rolling(random);
+        // console.log(random);
     }
-
-    if(hours >= 12){
-        hours = hours - 12;
-        ampm = 'PM';
+    else
+    {
+        random_gen();
     }
-    else{
-        ampm = 'AM';
-    }
-    if(hours == 0){ hours=12} ;
-    
-    inner_part.insertAdjacentHTML("afterbegin",`<div class='day-date'>${dates} | ${day}</div>`);
-    inner_part.insertAdjacentHTML("afterbegin",`<div class='ampm'>${ampm}</div>`);
 }
 
-timetext()
-setInterval((()=>{
-    time();
-}),1000)
-
+roll_btn.addEventListener('click',random_gen);
